055c9fb48c8dad90e9a2ead54cf948b1f04fc193
VivianMak <vivianmak.30@gmail.com>
2025-10-21 23:00:26 -0400

    brainstorming visualizations

---
+++ src/visualizations.py
@@ -0,0 +1,21 @@
+"""Generate different visualizations throughout game."""
+
+# For decision trees
+import matplotlib.pyplot as plt
+from sklearn import tree
+
+
+def initial_gamestate():
+    """Show the initialzation of obstacles, pursuer, and evader."""
+    # visualization should be a grid
+        # white = no obstacle
+        # black = obstacle
+        # red/blue = evader
+        # green = pursuer
+    pass
+
+def minimax():
+    """Show graph tree of decisions populated with values."""
+    pass
+
+
