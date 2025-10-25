82a36bd6f5686b2d7e87d00cd53e9ad6e104c358
VivianMak <vivianmak.30@gmail.com>
2025-10-25 17:19:26 -0400

    gamestate visualization finished

--- src/visualizations.py
+++ src/visualizations.py
@@ -1,18 +1,57 @@
 """Generate different visualizations throughout game."""
 
-# For decision trees
 import matplotlib.pyplot as plt
+from matplotlib import colors
+import numpy as np
+import random
 from sklearn import tree
 
+from utils import Occupancy
 
-def initial_gamestate():
-    """Show the initialzation of obstacles, pursuer, and evader."""
-    # visualization should be a grid
-        # white = no obstacle
-        # black = obstacle
-        # red/blue = evader
-        # green = pursuer
-    pass
+
+def gamestate_visual(graph, size, filename="../images/graph"):
+    """
+    Show plot of environment containing obstacles, pursuer, evader.
+    
+    Args:
+        graph: an np array with initalized obstacles and agents
+        size: the size n by n graph
+    """   
+    # Using it
+    # env = Environment(10, 0.2, CellIndex(2,2), CellIndex(8,8))
+    # # Save image for each ith turn
+    # # filename = "../images/graph_" + str(i)
+    # gamestate_visual(env._graph, env._size)
+
+    # Convert Enums to integers if needed
+    # if graph.dtype == object:
+    #     graph = np.vectorize(lambda x: x.value if isinstance(x, Occupancy) else x)(graph)
+
+
+    cmap = colors.ListedColormap([
+        'white',    # Occupancy.EMPTY
+        'black',    # Occupancy.OBSTACLE
+        'green',    # Occupancy.PURSUANT
+        'red',      # Occupancy.EVADER
+        ])
+    bounds = [0,1,2,3,4]
+    norm = colors.BoundaryNorm(bounds, cmap.N)
+
+    fig, ax = plt.subplots()
+    ax.imshow(graph, cmap=cmap, norm=norm)
+
+    # Draw gridlines
+    ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=0.1)
+    
+    # Make sure each cell center is 0.5 above value
+    ax.set_xticks(np.arange(0.5, size, 1))
+    ax.set_yticks(np.arange(0.5, size, 1))
+
+    # Hide tick values
+    plt.tick_params(axis='both', which='both', bottom=False,   
+                    left=False, labelbottom=False, labelleft=False) 
+    
+    plt.show()
 
 def minimax():
     """Show graph tree of decisions populated with values."""
